22501928032bdeb1bd149bd0d12b025d54d4d18a
Shegofa <shogofadeveloper12@gmail.com>
2024-07-04 01:26:50 +0400

    some edit

--- script.js
+++ script.js
@@ -3,21 +3,36 @@ document.addEventListener("DOMContentLoaded", function () {
   const checkBtn = document.getElementById("valid");
   const clearBtn = document.getElementById("reset");
   const resultsDiv = document.getElementById("para");
-  const usa = document.getElementById("select-area");
+  const selectedOption = document.getElementById("select-area");
   checkBtn.addEventListener("click", function () {
-    if (usa.selectedIndex == 2) {
-      const phoneNumber = userInput.value.trim();
-      if (phoneNumber === "") {
-        alert("Please provide a phone number");
-        return;
-      }
-
+    const phoneNumber = userInput.value.trim();
+    if (phoneNumber === "") {
+      alert("Please provide a phone number");
+      return;
+    }
+    if (selectedOption.selectedIndex == 2) {
       const isValid = validateUSPhoneNumber(phoneNumber);
 
       if (isValid) {
         resultsDiv.textContent = `Valid US number: ${phoneNumber}`;
       } else {
         resultsDiv.textContent = `Invalid US number: ${phoneNumber}`;
+      }
+    } else if (selectedOption.selectedIndex == 0) {
+      const isValid = validateAFPhoneNumber(phoneNumber);
+
+      if (isValid) {
+        resultsDiv.textContent = `Valid Afghan number: ${phoneNumber}`;
+      } else {
+        resultsDiv.textContent = `Invalid Afghan number: ${phoneNumber}`;
+      }
+    } else {
+      const isValid = validateAFPhoneNumber(phoneNumber);
+
+      if (isValid) {
+        resultsDiv.textContent = `Valid Iran number: ${phoneNumber}`;
+      } else {
+        resultsDiv.textContent = `Invalid Iran number: ${phoneNumber}`;
       }
     }
   });
@@ -28,6 +43,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function validateUSPhoneNumber(phoneNumber) {
     const regex = /^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})[- )]?\d{3}[- ]?\d{4}$/;
+    return regex.test(phoneNumber);
+  }
+
+  function validateAFPhoneNumber(phoneNumber) {
+    const regex = /^((\+*)93|0093)?0?(7[0-9]{8})$/;
+    return regex.test(phoneNumber);
+  }
+
+  function validateAFPhoneNumber(phoneNumber) {
+    const regex = /^(?:\+98|0)?9\d{9}$/;
     return regex.test(phoneNumber);
   }
 });
