bdf2063fffbc901280e2ed142f03c817c6008510
Shegofa <shogofadeveloper12@gmail.com>
2024-07-04 01:59:51 +0400

    script.js file change to freecodecamp reqiured

--- script.js
+++ script.js
@@ -1,58 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const userInput = document.getElementById("num");
-  const checkBtn = document.getElementById("valid");
-  const clearBtn = document.getElementById("reset");
-  const resultsDiv = document.getElementById("para");
-  const selectedOption = document.getElementById("select-area");
-  checkBtn.addEventListener("click", function () {
-    const phoneNumber = userInput.value.trim();
-    if (phoneNumber === "") {
-      alert("Please provide a phone number");
-      return;
-    }
-    if (selectedOption.selectedIndex == 2) {
-      const isValid = validateUSPhoneNumber(phoneNumber);
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
 
-      if (isValid) {
-        resultsDiv.textContent = `Valid US number: ${phoneNumber}`;
-      } else {
-        resultsDiv.textContent = `Invalid US number: ${phoneNumber}`;
-      }
-    } else if (selectedOption.selectedIndex == 0) {
-      const isValid = validateAFPhoneNumber(phoneNumber);
-
-      if (isValid) {
-        resultsDiv.textContent = `Valid Afghan number: ${phoneNumber}`;
-      } else {
-        resultsDiv.textContent = `Invalid Afghan number: ${phoneNumber}`;
-      }
-    } else {
-      const isValid = validateAFPhoneNumber(phoneNumber);
-
-      if (isValid) {
-        resultsDiv.textContent = `Valid Iran number: ${phoneNumber}`;
-      } else {
-        resultsDiv.textContent = `Invalid Iran number: ${phoneNumber}`;
-      }
-    }
-  });
-
-  clearBtn.addEventListener("click", function () {
-    resultsDiv.textContent = "";
-  });
-
-  function validateUSPhoneNumber(phoneNumber) {
-    const regex = /^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})[- )]?\d{3}[- ]?\d{4}$/;
-    return regex.test(phoneNumber);
-  }
-
-  function validateAFPhoneNumber(phoneNumber) {
-    const regex = /^((\+*)93|0093)?0?(7[0-9]{8})$/;
-    return regex.test(phoneNumber);
-  }
-
-  function validateAFPhoneNumber(phoneNumber) {
-    const regex = /^(?:\+98|0)?9\d{9}$/;
-    return regex.test(phoneNumber);
+checkBtn.addEventListener("click", () => {
+  const input = userInput.value;
+  if (input.length === 0) {
+    alert("Please provide a phone number");
+  } else {
+    checkValidNumber(input);
+    userInput.value = "";
   }
 });
+
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkValidNumber(userInput.value);
+    userInput.value = "";
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  resultsDiv.innerHTML = "";
+});
+
+function checkValidNumber(input) {
+  const countryCode = "^(1\\s?)?";
+  const areaCode = "(\\([0-9]{3}\\)|[0-9]{3})";
+  const spacesDashes = "[\\s\\-]?";
+  const phoneNumber = "[0-9]{3}[\\s\\-]?[0-9]{4}$";
+  const phoneRegex = new RegExp(
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+  );
+  phoneRegex.test(input)
+    ? (resultsDiv.style.color = "#066637")
+    : (resultsDiv.style.color = "#af0f0f");
+  resultsDiv.innerHTML = `${
+    phoneRegex.test(input) ? "Valid" : "Invalid"
+  } US number: ${input}`;
+}
